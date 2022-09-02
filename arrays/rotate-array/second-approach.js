b5a13a80c340c45c3dca2d42c27d546dfe52f5f8
Chinedu Abalogu <chineduabalogu@gmail.com>
2022-09-02 15:12:22 +0100

    second iterative approach

---
+++ arrays/rotate-array/second-approach.js
@@ -0,0 +1,35 @@
+const binarySearchRotated = (arr, target) => {
+  let start = 0;
+  let end = arr.length - 1;
+
+  if (start > end) {
+    return -1;
+  }
+
+  while (start <= end) {
+    let mid = Math.floor(end - start / 2);
+
+    if (target === arr[mid]) {
+      return mid;
+    }
+
+    // check if right or left side of array is sorted
+    if (arr[start] <= arr[mid]) {
+      if (arr[mid] > target && arr[start] < target) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    } else {
+      if (arr[mid] < target && arr[end] > target) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+  }
+
+  return -1;
+};
+
+console.log(binarySearchRotated([6, 7, 1, 2, 3, 4, 5], 3));
