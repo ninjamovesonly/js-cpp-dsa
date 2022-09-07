e7e0b47d99b58d8a80e727cb146f4783bf0f53cb
Chinedu Abalogu <chineduabalogu@gmail.com>
2022-09-07 15:38:06 +0100

    first approach not working for negative rotate amount

---
+++ arrays/rotating-array/first-approach.js
@@ -0,0 +1,40 @@
+// does not work for negative rotateAmount
+const rotateArray = (originalArray, rotateAmount) => {
+  const reversedArray = reverseArrayUtilityFunction(originalArray);
+  const arrayLength = originalArray.length - 1;
+  if (rotateAmount > originalArray.length) {
+    rotateAmount = rotateAmount % arrayLength;
+  }
+  const absRotateAmount = Math.abs(rotateAmount);
+
+  reversedArray.splice(
+    0,
+    absRotateAmount,
+    ...reverseArrayUtilityFunction(originalArray.slice(0, absRotateAmount))
+  );
+
+  reversedArray.splice(
+    absRotateAmount,
+    arrayLength,
+    ...reverseArrayUtilityFunction(originalArray.slice(absRotateAmount))
+  );
+
+  return reversedArray;
+};
+
+function reverseArrayUtilityFunction(arrayToReverse) {
+  let p1 = 0;
+  let p2 = arrayToReverse.length - 1;
+  while (p2 > p1) {
+    const temp = arrayToReverse[p1];
+    arrayToReverse[p1] = arrayToReverse[p2];
+    arrayToReverse[p2] = temp;
+    p1++;
+    p2--;
+  }
+  return arrayToReverse;
+}
+
+console.log(rotateArray([1, 10, 20, 0, 59, 86, 32, 11, 9, 40], -3)); // wrong output :(
+console.log(rotateArray([1, 10, 20, 0, 59, 86, 32, 11, 9, 40], 3));
+console.log(rotateArray([1, 10, 20, 0, 59, 86, 32, 11, 9, 40], 2));
