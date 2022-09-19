155aeaa54d7b687cb9b01cb632f1922671d83f9f
Chinedu Abalogu <chineduabalogu@gmail.com>
2022-09-19 14:38:45 +0100

    solved

--- leetcode/find-original-array-2007.js
+++ leetcode/find-original-array-2007.js
@@ -1,83 +1,58 @@
-// incorrect solution
+// correct solution
 var findOriginalArray = function (changed) {
-  let changedLength = changed.length,
-    changedLengthHalf = changedLength / 2,
-    result = [],
-    result2 = [],
-    elementCountMap = {},
-    map = {},
-    elementCountMap2 = {},
-    map2 = {};
+  let count = Counter(changed),
+    changedLength = changed.length,
+    result = [];
 
-  if (changedLength % 2 === 0) {
-    for (let i = 0; i < changedLength; i++) {
-      let count = 0;
-      const elementDouble = changed[i] * 2;
-      if (i < changedLengthHalf) {
-        for (let j = changedLengthHalf; j < changedLength; j++) {
-          if (changed[j] === elementDouble) {
-            count++;
-          }
-        }
-        if (count) {
-          if (map[changed[i]]) {
-            map[changed[i]]++;
-          } else {
-            map[changed[i]] = 1;
-          }
-        }
-        if (elementCountMap[changed[i]]) {
-          elementCountMap[changed[i]]++;
-        } else {
-          elementCountMap[changed[i]] = 1;
-        }
-        for (const property in map) {
-          if (map[property] === elementCountMap[property]) {
-            result.push(Number(property));
-          }
-        }
-      }
-      elementCountMap = {};
-      map = {};
-      // if (result.length !== changedLengthHalf) {
-      //   result = [];
-      // }
-      if (i >= changedLengthHalf && result.length !== changedLengthHalf) {
-        for (let j = 0; j < changedLengthHalf; j++) {
-          if (changed[j] === elementDouble) {
-            count++;
-          }
-        }
-        if (count) {
-          if (map2[changed[i]]) {
-            map2[changed[i]]++;
-          } else {
-            map2[changed[i]] = 1;
-          }
-        }
-        if (elementCountMap2[changed[i]]) {
-          elementCountMap2[changed[i]]++;
-        } else {
-          elementCountMap2[changed[i]] = 1;
-        }
-        for (const property in map2) {
-          if (map2[property] === elementCountMap2[property]) {
-            result2.push(Number(property));
-          }
-        }
-        if (result.length !== changedLengthHalf) {
-          result = [];
-        }
-      }
+  if (changedLength % 0) {
+    return result;
+  }
+  changed.sort((a, b) => +a - +b);
+  for (item of changed) {
+    if (item === 0 && count[item] >= 2) {
+      count[item] -= 2;
+      result.push(item);
+    } else if (item > 0 && count[item] && count[item * 2]) {
+      count[item]--;
+      count[item * 2]--;
+      result.push(item);
     }
   }
-  console.log("result2..", result2);
+
+  if (result.length !== changedLength / 2) {
+    console.log("result... ", result);
+    return [];
+  }
+
   return result;
 };
 
-console.log(findOriginalArray([1, 4, 2, 1])); // fails for this edge case
-console.log(findOriginalArray([2, 2, 1, 1]));
-console.log(findOriginalArray([4, 0, 3, 0]));
-console.log(findOriginalArray([0, 0, 0, 0]));
-console.log(findOriginalArray([2, 1]));
-console.log(findOriginalArray([1, 3, 4, 2, 6, 8]));
+const Counter = (arr) => {
+  let count = {};
+  for (element of arr) {
+    if (count[element]) {
+      count[element]++;
+    } else {
+      count[element] = 1;
+    }
+  }
+  return count;
+};
+
+console.log(
+  findOriginalArray([
+    40, 7, 78, 12, 40, 28, 33, 27, 35, 90, 56, 44, 42, 38, 36, 3, 12, 68, 86,
+    14, 27, 80, 33, 40, 12, 74, 20, 50, 15, 54, 76, 13, 40, 3, 43, 88, 14, 54,
+    20, 0, 100, 10, 23, 30, 27, 50, 84, 24, 15, 45, 94, 66, 6, 22, 20, 34, 25,
+    100, 28, 6, 37, 10, 18, 82, 96, 0, 76, 40, 32, 33, 48, 70, 24, 80, 20, 40,
+    50, 4, 19, 25, 66, 38, 46, 44, 98, 47, 26, 54, 38, 39, 41, 20, 49, 8, 16, 6,
+    50, 30, 20, 66,
+  ])
+);
+// console.log(findOriginalArray([4, 4, 16, 20, 8, 8, 2, 10]));
+// console.log(findOriginalArray([1, 4, 2, 1]));
+// console.log(findOriginalArray([2, 2, 1, 1]));
+// console.log(findOriginalArray([4, 0, 3, 0]));
+// console.log(findOriginalArray([0, 0, 0, 0]));
+// console.log(findOriginalArray([2, 1]));
+// console.log(findOriginalArray([1, 3, 4, 2, 6, 8]));
