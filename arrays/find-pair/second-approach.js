4104091f1db22cf0a1618a85f88875f1723f3e21
Chinedu Abalogu <chineduabalogu@gmail.com>
2022-09-19 17:15:34 +0100

    using a hashset

--- arrays/find-pair/second-approach.js
+++ arrays/find-pair/second-approach.js
@@ -1,1 +1,16 @@
-// to write optimised approach
+let findSumOfTwo = function (nums, val) {
+  let set = new Set();
+
+  for (let item of nums) {
+    console.log(item);
+    if (set.has(val - item)) {
+      return true;
+    }
+
+    set.add(item);
+  }
+
+  return false;
+};
+
+console.log(findSumOfTwo([2, 1, 8, 4, 7, 3], 3));
