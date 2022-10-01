45e66e17eb88640772ee64ae2e4060aa58fc7439
Chinedu Abalogu <chineduabalogu@gmail.com>
2022-10-01 13:20:01 +0100

    more practice updates

---
+++ graphs/dfs.js
@@ -0,0 +1,60 @@
+class Graph {
+  constructor(v) {
+    this.vertices = v;
+    this.edges = 0;
+    this.adj = [];
+    for (var i = 0; i < this.vertices; i++) {
+      this.adj[i] = [];
+      this.adj[i].push("");
+    }
+
+    this.addEdge = addEdge;
+    this.showGraph = showGraph;
+    this.bfs = bfs;
+    this.marked = [];
+
+    for (var i = 0; i < this.vertices; i++) {
+      this.marked[i] = false;
+    }
+  }
+}
+
+function addEdge(v, w) {
+  this.adj[v].push(w);
+  this.adj[w].push(v);
+  this.edges++;
+}
+
+function showGraph() {
+  for (var i = 0; i < this.vertices; i++) {
+    console.log(i + " => ");
+    for (var j = 0; j < this.vertices; j++) {
+      if (this.adj[i][j] != undefined) {
+        console.log(this.adj[i][j] + "");
+      }
+    }
+    console.log("");
+  }
+}
+
+function dfs(v) {
+  this.marked[v] = true;
+  if (this.adj[v] != undefined) {
+    console.log("visited vertex: ", v);
+  }
+
+  var w = "";
+  for (var w in this.adj[v]) {
+    if (!this.marked[w]) {
+      this.dfs(w);
+    }
+  }
+}
+
+g = new Graph(5);
+g.addEdge(0, 1);
+g.addEdge(0, 2);
+g.addEdge(1, 3);
+g.addEdge(2, 4);
+g.showGraph();
+g.dfs(0);
