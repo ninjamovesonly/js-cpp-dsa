343796075bc222fee50b46bc6506def46b89a3af
Chinedu Abalogu <chineduabalogu@gmail.com>
2022-10-14 00:10:37 +0100

    solved leetcode 2423

---
+++ leetcode/find-rotation-1886.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[][]} mat
+ * @param {number[][]} target
+ * @return {boolean}
+ */
+var findRotation = function (mat, target) {};
+
+console.log(
+  [
+    [0, 1],
+    [1, 0],
+  ],
+  [
+    [1, 0],
+    [0, 1],
+  ]
+);
+
+console.log(
+  [
+    [0, 0, 0],
+    [0, 1, 0],
+    [1, 1, 1],
+  ],
+  [
+    [1, 1, 1],
+    [0, 1, 0],
+    [0, 0, 0],
+  ]
+);
